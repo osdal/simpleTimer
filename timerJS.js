5d891495ac9d1fbf46a0c4ba6fe49cf9afcd00c8
osdal <osdal67@gmail.com>
2018-08-20 18:13:47 +0300

    first

---
+++ timerJS.js
@@ -0,0 +1,13 @@
+﻿		let timerId = setInterval(simpleTimer, 1000);
+			
+
+	function simpleTimer () {
+				 
+		let time = new Date(),
+			timer = document.getElementById('timer'),
+			reg = /\d\d:\d\d:\d\d/;
+			time = time + '';
+			time = time.match(reg);
+
+			timer.textContent = time;
+	}
